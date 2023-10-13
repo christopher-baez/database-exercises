7bc831a0dc535356b98966f3fd0a5ebf3eb69c33
christopher.baez <christopherbaez8@gmail.com>
2023-10-13 15:43:12 -0500

    odified answers

--- limit_exercise.sql
+++ limit_exercise.sql
@@ -1,17 +1,23 @@
 use employees;
+-- List the first 10 distinct last names sorted in descending order.
+select distinct * from employees 
+order by last_name desc limit 10;
 -- Find all previous or current employees hired in the 90s and born on Christmas.
 --  Find the first 5 employees hired in the 90's by sorting by hire date and limiting
 --  your results to the first 5 records. Write a comment in your code that lists the
 --  five names of the employees returned.
 select * from employees
-where hire_date between '1990-01-01' and '1999-12-31' and hire_date like '%12-25'
+where hire_date between '1990-01-01' and '1999-12-31' and birth_date like '%12-25'
 order by hire_date limit 5;
+-- yinghua dredge, jinpo langford, tina speek, takahira spataro, jaber piveteau
+
 
 -- Try to think of your results as batches, sets, or pages. 
 -- The first five results are your first page. The five after that would be your second page, etc. 
 -- Update the query to find the tenth page of results.
 select * from employees
-where hire_date between '1990-01-01' and '1999-12-31' and hire_date like '%12-25'
+where hire_date between '1990-01-01' and '1999-12-31' and birth_date like '%12-25'
 order by hire_date 
-limit 5 offset 10;
+limit 5 offset 45;
 
+-- (number od results per page * number of pages) - result of one page
--- where_advance_exercise.sql
+++ where_advance_exercise.sql
@@ -5,7 +5,7 @@ show tables;
 ## 'Irena', 'Vidya', or 'Maya' using IN. What is the employee 
 ## number of the top three results?
 SELECT * from employees
-where first_name in ("vidya", "Maya", "Irene"); -- 10200, 10256, and 10360
+where first_name in ("vidya", "Maya", "Irena"); -- 10200, 10397, and 10610
 
 ## Find all current or previous employees with first names 
 ## 'Irena', 'Vidya', or 'Maya', as in Q2, but use OR instead of IN. 
@@ -14,14 +14,14 @@ SELECT * from employees
 where 
 first_name = "vidya" or 
 first_name = "Maya" or 
-first_name = "Irene"; -- same as last question (10200, 10256, and 10360)
+first_name = "Irena"; -- same as last question (10200, 10397, and 10610)
 
 ## Find all current or previous employees with first names 
 ## 'Irena', 'Vidya', or 'Maya', using OR, and who is male. 
 ## What is the employee number of the top three results?
 SELECT * from employees
-where (first_name = "vidya" or first_name = "Maya" or first_name = "Irene") 
-and gender = "M"; -- same as before (10200, 10256, and 10360)
+where (first_name = "vidya" or first_name = "Maya" or first_name = "Irena") 
+and gender = "M"; -- 10200, 10397, 10821
 
 ## Find all unique last names that start with 'E'.
 select distinct last_name from employees
@@ -37,22 +37,21 @@ where last_name not like "E%" and last_name like "%E";
 
 ##Find all unique last names that start and end with 'E'.
 select distinct last_name from employees
-where last_name like "E%" and last_name like "%E";
+where last_name like "E%E";
 
 ##Find all current or previous employees hired in the 90s. 
 ##Enter a comment with the top three employee numbers.
 select * from employees
-where hire_date between '1990-01-01' and '1999-12-31'; -- 10008, 10011, 10012
+where hire_date like "199%";-- 10008, 10011, 10012
 
 ##Find all current or previous employees born on Christmas. 
 ##Enter a comment with the top three employee numbers.
 select * from employees
-where hire_date like '%12-25'; -- 10050,10456, 10463
-
+where birth_date like '%12-25'; -- 10078,10115,10261
 ##Find all current or previous employees hired in the 90s and born on Christmas. 
 ##Enter a comment with the top three employee numbers.
 select * from employees
-where hire_date between '1990-01-01' and '1999-12-31' and hire_date like '%12-25'; -- 10050,10456, 10517
+where hire_date like "199%" and birth_date like '%12-25'; -- 110261, 10438, 10681
 
 ##Find all unique last names that have a 'q' in their last name.
 select distinct last_name from employees
